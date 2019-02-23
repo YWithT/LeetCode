4634c6e0ea47635a588aefb2b90d7354d156a527
我摘青橘 <shucesyc@163.com>
2019-02-23 22:44:43 +0800

    继续努力

--- 542_01矩阵.js
+++ 542_01矩阵.js
@@ -1,42 +1,90 @@
+
+
 /**
- * @param {number[][]} matrix
+ * @param {number[][]} resMatrix
  * @return {number[][]}
  */
-var updateMatrix = function(matrix) {
+
+ /**
+  * 算法思想：
+  * 1. 第一次遍历，将0四周的1全部改为-1；
+  * 2. 第二次遍历，将-1四周的1全部改为-2；
+  * 3. 以此类推，直到遍历结束后矩阵未发生变化
+  * 4. 将矩阵中的值取反并返回
+  * 
+  * 缺点：多次二重循环，效率不高，BFS算法效率更佳
+  */
+var updateMatrix = function (matrix) {
     var row = matrix.length;
     var column = matrix[0].length;
-    var resMatrix = new Array(row).fill(new Array(column).fill(-1));
-    for(let i = 0;;i++){
-        for(let [rowIndex,rowArray] of matrix.entries()){
-            for(let [columnIndex,val] of rowArray.entries()){
-                if(val == i){
-                    changeMatrix(resMatrix,i,rowIndex,columnIndex);
+
+    for (let val = 0;; val--) {
+        let flag = false;
+        for (let i = 0; i < row; i++) {
+            for (let j = 0; j < column; j++) {
+                if (matrix[i][j] == val) {
+                    flag = changeAround(matrix, val, i, j, row, column) == true ? true : flag;
                 }
-            } 
+            }
+        }
+        if (flag == false) {
+            break;
         }
     }
-    return resMatrix;
+
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < column; j++) {
+            matrix[i][j] = -matrix[i][j];
+        }
+    }
+
+    return matrix;
 };
 
-function matrixIndexof(matrix,value){
-    for(let [index, array] of matrix.entries()){
-        if(array.includes(value)) {
-            return [index,array.indexOf(value)];
-        }
-    };
-    return [-1,-1];
+function changeAround(matrix, val, i, j, row, column) {
+    let flag = false;
+    if (i - 1 > -1 && matrix[i - 1][j] == 1) {
+        matrix[i - 1][j] = val - 1;
+        flag = true;
+    }
+    if (i + 1 < row && matrix[i + 1][j] == 1) {
+        matrix[i + 1][j] = val - 1;
+        flag = true;
+    }
+    if (j - 1 > -1 && matrix[i][j - 1] == 1) {
+        matrix[i][j - 1] = val - 1;
+        flag = true;
+    }
+    if (j + 1 < column && matrix[i][j + 1] == 1) {
+        matrix[i][j + 1] = val - 1;
+        flag = true;
+    }
+    return flag;
 }
 
-function changeMatrix(matrix,val,rowIndex,columnIndex){
-    if(matrix[rowIndex+1][columnIndex] === -1)
-        matrix[rowIndex+1][columnIndex] = matrix[rowIndex+1][columnIndex] === 0 ? 0 : val + 1; 
-    if(matrix[rowIndex-1][columnIndex] === -1)
-        matrix[rowIndex-1][columnIndex] = matrix[rowIndex-1][columnIndex] === 0 ? 0 : val + 1; 
-    if(matrix[rowIndex][columnIndex+1] === -1)
-        matrix[rowIndex][columnIndex+1] = matrix[rowIndex][columnIndex+1] === 0 ? 0 : val + 1; 
-    if(matrix[rowIndex][columnIndex-1] === -1)
-        matrix[rowIndex][columnIndex-1] = matrix[rowIndex][columnIndex-1] === 0 ? 0 : val + 1; 
-}
+let a = [
+    [1, 0, 1, 1, 0, 0, 1, 0, 0, 1],
+    [0, 1, 1, 0, 1, 0, 1, 0, 1, 1],
+    [0, 0, 1, 0, 1, 0, 0, 1, 0, 0],
+    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1],
+    [0, 1, 0, 1, 1, 0, 0, 0, 0, 1],
+    [0, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0, 0, 1, 1],
+    [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+    [1, 1, 1, 1, 0, 1, 0, 0, 1, 1]
+]
 
-let a = [[0,0,0],[0,1,0],[1,1,1]]
-console.log(updateMatrix(a));
+let b = updateMatrix(a)
+
+console.log(b);
+
+
+// function resMatrixIndexof(resMatrix, value) {
+//     for (let [index, array] of resMatrix.entries()) {
+//         if (array.includes(value)) {
+//             return [index, array.indexOf(value)];
+//         }
+//     };
+//     return false;
+// }
--- test.js
+++ test.js
@@ -1,12 +1,2 @@
-function matrixIndexof(matrix,value){
-    for(let [index, array] of matrix.entries()){
-        if(array.includes(value)) {
-            return [index,array.indexOf(value)];
-        }
-    };
-    return [-1,-1];
-}
-
-let a = [[1,2,3],[4,5,6],[7,8,9]];
-let b = 10;
-console.log(a[4][3])
+var a = [[1, 0, 1, 1, 0, 0, 1, 0, 0, 1], [0, 1, 1, 0, 1, 0, 1, 0, 1, 1], [0, 0, 1, 0, 1, 0, 0, 1, 0, 0], [1, 0, 1, 0, 1, 1, 1, 1, 1, 1], [0, 1, 0, 1, 1, 0, 0, 0, 0, 1], [0, 0, 1, 0, 1, 1, 1, 0, 1, 0], [0, 1, 0, 1, 0, 1, 0, 0, 1, 1], [1, 0, 0, 0, 1, 1, 1, 1, 0, 1], [1, 1, 1, 1, 1, 1, 1, 0, 1, 0], [1, 1, 1, 1, 0, 1, 0, 0, 1, 1]];
+console.log(a);
