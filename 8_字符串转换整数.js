11434095eb81a86d9425b3a3b0dda17ce9ad962a
YWithT <shucesyc@163.com>
2019-02-25 14:33:40 +0800

    parseInt函数是从第一个非空字符开始转换而不是第一个数字字符开始！

--- 8_字符串转换整数.js
+++ 8_字符串转换整数.js
@@ -2,9 +2,18 @@
  * @param {string} str
  * @return {number}
  */
+
+//算法核心：parseInt函数，从字符串的第一个非空字符开始，一直转换到第一个非数字字符。若第一个非空字符即为非数字字符则返回NaN
+//if(NaN) 返回false
 var myAtoi = function (str) {
-    str = str.trim();
-    if (str[0] != '+' || str[0] != '-' || str[0] < '0' || str[0] > '9')
+        var res = parseInt(str);
+        if (res) {
+                if (res < -2147483648) return -2147483648;
+                if (res > 2147483647) return 2147483647;
+                return res;
+        }
         return 0;
-    
+
 };
+
+console.log(myAtoi('w34'));
