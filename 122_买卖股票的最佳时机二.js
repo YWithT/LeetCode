9e697427a6dd031561105c4acf7f67c777d3bd89
我摘青橘 <shucesyc@163.com>
2019-02-25 21:56:00 +0800

    优化算法思想，简化代码结构

--- 122_买卖股票的最佳时机二.js
+++ 122_买卖股票的最佳时机二.js
@@ -3,15 +3,40 @@
  * @return {number}
  */
 
+//借鉴写法
+/**
+ * 算法思想:只需要从第二天开始，如果当前价格比之前价格高，则把差值加入利润中，因为我们可以昨天买入，今日卖出，
+ * 若明日价更高的话，还可以今日买入，明日再抛出。以此类推，遍历完整个数组后即可求得最大利润。 
+ * */
+var maxProfit = function (prices) {
+    var profit = 0;
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices[i - 1];
+        }
+    }
+    return profit;
+};
+var a = [7, 1, 5, 3, 6, 4];
+console.log(maxProfit(a));
 
- //自己的写法，太丑了
+
+
+
+
+
+
+//自己的写法，想的比较复杂，写法较丑
+//算法思想：制定3个下标i，j=i+1，k=j+1，k用来探测，如果下标k的值比下标j的值小，则计算当前利润，并在下一天买入
+//若下一位的值比下标j的值大则j和k加1.再重复上述步骤
 var maxProfit = function (prices) {
     var profit = 0;
     for (let i = 0, j = 1; j < prices.length;) {
         if (prices[j] <= prices[i]) {
-            i++; j++; continue;
-        }
-        else if (prices[j] > prices[i]) {
+            i++;
+            j++;
+            continue;
+        } else if (prices[j] > prices[i]) {
             if (j + 1 < prices.length) {
                 for (let k = j + 1; k < prices.length; k++) {
                     if (prices[k] < prices[j]) {
@@ -29,8 +54,7 @@ var maxProfit = function (prices) {
                         continue;
                     }
                 }
-            }
-            else {
+            } else {
                 profit += prices[j] - prices[i];
                 j++;
             }
@@ -38,6 +62,3 @@ var maxProfit = function (prices) {
     }
     return profit;
 };
-
-var a = [7,1,5,3,6,4];
-console.log(maxProfit(a));
