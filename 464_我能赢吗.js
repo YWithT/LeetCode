5e5660ed6555a3941072bd209d530d4d06ef3330
YWithT <shucesyc@163.com>
2019-04-17 14:59:27 +0800

    为464添加注释，理解位标记法

--- 464_我能赢吗.js
+++ 464_我能赢吗.js
@@ -11,55 +11,27 @@ var canIWin = function (maxChoosableInteger, desiredTotal) {
 };
 
 function canWin(maxInt, total, used, res) {
-    if (res[used]) return res[used];
+    //使用的是res[used]而不是res[total]，这样可以同时记录状态和该状态对应使用了哪些数字
+    //例如res[7] = ture,表示在使用了二进制数字111，即数字1,2,3时，先手能赢。
+    if (res[used]) {
+        return res[used];
+    }
     for (let i = 0; i < maxInt; i++) {
+        //将cur左移i位后与used按位进行与运算，若没有重复数字则结果为0
         let cur = (1 << i);
         let cal = cur & used;
-        if (cal == 0) { //按位与
+
+        if (cal == 0) {
+            //若下一步对手输即先手为赢，所以当下一步返回false时或者i+1大于total时，对应的used为true
             if (total <= i + 1 || !canWin(maxInt, total - (i + 1), used | cur, res)) {
                 res[used] = true;
                 return true;
             }
         }
     }
+    //若返回的全为true表示下一步对手必胜，则此步为false
     res[used] = false;
     return false;
 };
 
-
-//错误解法：此题无法从下往上计算，因为无法记住在上一个状态使用过哪些数字
-// var canIWin = function (maxChoosableInteger, desiredTotal) {
-//     if (maxChoosableInteger >= desiredTotal) return true;
-//     if ((maxChoosableInteger + 1) / 2 * maxChoosableInteger < desiredTotal) return false;
-//     let res = [];
-//     let visited = new Array(desiredTotal + 1).fill(0);
-//     for (let i = 0; i <= desiredTotal; i++) {
-//         canWin(i, maxChoosableInteger, visited, res);
-//     }
-//     for (let i in res)
-//         console.log(i, res[i]);
-//     return res[desiredTotal];
-// };
-
-// function canWin(cur, maxInt, visited, res) {
-//     if (res[cur] != undefined) return res[cur];
-//     if (cur <= maxInt) {
-//         res[cur] = true;
-//         return;
-//     }
-//     for (let i = 1; i <= maxInt; i++) {
-//         if (visited[i] != 1) {
-//             visited[i] = 1;
-//             if (!canWin(cur - i, maxInt, visited, res)) {
-//                 res[cur] = true;
-//                 visited[i] = 0;
-//                 return;
-//             }
-//             visited[i] = 0;
-//         }
-//     }
-//     res[cur] = false;
-//     return;
-// };
-
-console.log(canIWin(10, 40))
+console.log(canIWin(4, 8))
