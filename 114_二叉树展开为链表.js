63b2007594324026cab013afe85d0929c34254d4
YWithT <shucesyc@163.com>
2019-06-25 13:19:47 +0800

    继续努力

---
+++ 114_二叉树展开为链表.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function (root) {
+    if (root == null) return;
+    flatten(root.left);
+    flatten(root.right);
+    let node = root.right;
+    root.right = root.left;
+    root.left = null;
+    while (root.right != null) root = root.right;
+    root.right = node;
+};
